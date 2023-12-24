5fc142d9b14be933d045967a7cd88324a7e6972d
hglong16 <intihad.vuong@gmail.com>
2023-12-24 21:56:39 +0700

    Update code formatting and fix minor bugs

---
+++ fastapi_supabase/app.py
@@ -0,0 +1,69 @@
+from importlib import metadata
+from pathlib import Path
+
+from fastapi import FastAPI
+from fastapi.responses import UJSONResponse
+from fastapi.staticfiles import StaticFiles
+
+from fastapi_supabase.api.api_router import api_router
+
+from fastapi.middleware.cors import CORSMiddleware
+
+APP_ROOT = Path(__file__).parent.parent
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "*",
+]
+
+
+def get_app() -> FastAPI:
+    """
+    Get FastAPI application.
+
+    This is the main constructor of an application.
+
+    :return: application.
+    """
+    app = FastAPI(
+        title="fastapi_supabase",
+        version=metadata.version("fastapi_supabase"),
+        docs_url="/docs",
+        redoc_url="/redoc",
+        openapi_url="/api/openapi.json",
+        default_response_class=UJSONResponse,
+    )
+
+    # app = FastAPI(
+    #     title="s956",
+    #     version=metadata.version("s956"),
+    #     docs_url="/api/hglong/3357/docs",
+    #     redoc_url=None,
+    #     openapi_url=None,
+    #     default_response_class=UJSONResponse,
+    # )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    # Adds startup and shutdown events.
+
+    # Main router for the API.
+    app.include_router(router=api_router, prefix="/api")
+    # Adds static directory.
+    # This directory is used to access swagger files.
+    app.mount(
+        "/static",
+        StaticFiles(directory=APP_ROOT / "static"),
+        name="static",
+    )
+
+    return app
